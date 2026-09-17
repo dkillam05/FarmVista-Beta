91ac440186fd5377417698776ea4374d2165fef1
dkillam05 <danekillam@gmail.com>
2026-09-17 05:23:00 -0500

    Restore login Firebase compatibility path

---
+++ js/firebase-init.js
@@ -0,0 +1,3 @@
+// Compatibility entry point for modules that still resolve Firebase from /js/firebase-init.js.
+// Canonical implementation lives in /js/firebase/firebase-init.js.
+export * from "./firebase/firebase-init.js";
