80f4617fcf2392658750fa927db9c7a32ab1d26e
FarmVista Beta Organizer <actions@users.noreply.github.com>
2026-09-17 09:49:33 +0000

    Organize remaining JavaScript by feature

--- js/dashboard/markets/markets.js
+++ js/dashboard/markets/markets.js
@@ -1,5 +1,5 @@
 /* =====================================================================
-/js/markets.js  (FULL FILE)
+/js/dashboard/markets/markets.js  (FULL FILE)
 Rev: 2026-03-17a-true-expiration-date
 
 FIX (per Dane):
